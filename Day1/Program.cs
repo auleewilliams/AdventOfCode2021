962c6e2619eacec26d7da60720951d4d1b0a01de
Lee Williams <lee.williams@jjswaste.com.au>
2021-12-02 18:36:12 +1000

    Day 1 Part 1 Solution

---
+++ Day1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = File.ReadAllLines("C:/Code/AdventOfCode2021/Day1/input.txt");
+            var inputList = new List<string>(inputFile);
+            var depthMeasurementIncreases = 0;
+            var previousReading = 199;
+
+            foreach (var input in inputList)
+            {
+                var currentReading = int.Parse(input);
+
+                if (currentReading > previousReading)
+                    depthMeasurementIncreases++;
+
+                previousReading = currentReading;
+            }
+
+            Console.WriteLine($"Increases: {depthMeasurementIncreases}");
+        }
+    }
+}
